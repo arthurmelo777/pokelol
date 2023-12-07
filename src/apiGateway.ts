b02e6ec94f931decb936dc0f90f4bb92da3ff545
BrunoLTads <bruno4128@hotmail.com>
2023-12-07 07:58:41 -0300

    att: correção do gateway

--- src/apiGateway.ts
+++ src/apiGateway.ts
@@ -1,28 +1,30 @@
 const BASE_URL = {
-    pokemon: 'https://pokeapi.co/api/v2/',
-    digimon: 'https://digi-api.com/api/v1/'
-  };
-  
-  export const fetchRandomPokemon = async () => {
-    try {
-      const randomId = Math.floor(Math.random() * 898) + 1;
-      const response = await fetch(`${BASE_URL.pokemon}pokemon/${randomId}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Erro ao buscar Pokémon aleatório:', error);
-      throw new Error('Erro ao buscar Pokémon aleatório');
-    }
-  };
-  
-  export const fetchRandomDigimon = async () => {
-    try {
-      const randomDigiId = Math.floor(Math.random() * 1421) + 1;  //Vi que na API tem até o Dot Lalamon com id 1422
-      const response = await fetch(`${BASE_URL.digimon}digimon/${randomDigiId}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Erro ao buscar Digimon aleatório:', error);
-      throw new Error('Erro ao buscar Digimon aleatório');
-    }
-  };
+  pokemon: 'https://pokeapi.co/api/v2/',
+  digimon: 'https://digi-api.com/api/v1/'
+};
+
+type Endpoint = keyof typeof BASE_URL;
+
+// Abstração
+const fetchData = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    throw new Error('Erro ao buscar dados');
+  }
+};
+
+export const fetchRandomPokemon = async () => {
+  const randomId = Math.floor(Math.random() * 898) + 1; // Número de pokemons na PokéAPI. Se não me engano eles estão seguindo a ordem da Pokedex de cada geração (excluindo os pokemons já inseridos).
+  const url = `${BASE_URL.pokemon}pokemon/${randomId}`;
+  return fetchData(url);
+};
+
+export const fetchRandomDigimon = async () => {
+  const randomDigiId = Math.floor(Math.random() * 1421) + 1; // Número de digimons na DAPI, não sei a sequência certa pq pelo visto está utilizando pokemons fanmade também.
+  const url = `${BASE_URL.digimon}digimon/${randomDigiId}`;
+  return fetchData(url);
+};
